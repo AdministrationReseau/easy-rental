56ce4c55d42b269ce205c930eb2c69f885f600bb
whitecodename <ouendeufranck@gmail.com>
2024-11-16 06:05:15 +0100

    Finished Drivers components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-import StateBox from "@/components/StateBox";
-import Filter from "@/components/Filter";
-import CarProfilImg from "@/components/CarProfilImg";
-import CarProfilDescription from "@/components/CarProfilDescription";
-import { ProcessState } from "@/utils/enum";
-
 export default function Home() {
   return (
     <div className="">
       <main className="">
-        <CarProfilImg />
-        <CarProfilDescription />
-
-        <StateBox state={ProcessState.CONFIRMED} />
-        <StateBox state={ProcessState.PENDING} />
-        <StateBox state={ProcessState.CANCELED} />
-        <Filter />
 
       </main>
       <footer className="">
--- src/components/CarProfilImg.tsx
+++ src/components/CarProfilImg.tsx
@@ -1,26 +1,39 @@
+'use client'
+
 import * as React from 'react';
 import Image from 'next/image';
+import { useState } from 'react';
 
-export default function CarProfilImg() {
-    // let active_img_index = 0;
+interface CarProfilImgProps {
+    imagesPaths: string[], /* array of image paths names in the 'public' folder without the '/' start */
+    width: number,
+    height: number,
+}
 
-    const profil_path = 'car.png';
-    const images_paths = ['car.png', 'car2.png', 'car.png', 'car2.png', 'car.png', 'car2.png'];
-    const PROFIL_SIDE_SIZE = 300;
+export default function     CarProfilImg({ imagesPaths, width = 300, height = 500 }: CarProfilImgProps) {
+    const [activeImgIndex, setActiveImgIndex] = useState<number>(0);
+    const [profilPath, setProfilPath] = useState<string>(imagesPaths[0]);
+
+    const PROFIL_HEIGHT = 3 * height / 5;
     const MARGIN_BETWEEN_IMAGES = 2;
-    const MINIATURE = PROFIL_SIDE_SIZE / 3 - MARGIN_BETWEEN_IMAGES;
+    const MINIATURE = width / 3 - MARGIN_BETWEEN_IMAGES;
+
+    const handleMiniatureClick = (index: number) => {
+        setActiveImgIndex(index);
+        setProfilPath(imagesPaths[index]);
+    }
 
     return (
-        <div className={`w-[${PROFIL_SIDE_SIZE}px] m-2`}>
+        <div className={`w-[${width}px] m-2`}>
             {/* Main image */}
-            <div className='w-full'>
-                <Image src={`/${profil_path}`} alt="Car" width={PROFIL_SIDE_SIZE} height={PROFIL_SIDE_SIZE} className='rounded-md shadow-sm shadow-primary-blue/15'/>
+            <div className="relative rounded-md rounded-br-none rounded-bl-none shadow-sm shadow-primary-blue/15 pb-4 border-2 border-b-primary-blue" style={{ width: `${width}px`, height: `${PROFIL_HEIGHT}px`, overflow: "hidden", }}>
+                <Image src={`/${profilPath}`} alt="Car" layout="fill" className="object-contain" />
             </div>
 
             {/* Car Images in minature */}
-            <div className='scrollable flex flex-row gap-[${MARGIN_BETWEEN_IMAGES}] w-full overflow-x-auto scroll-co'>
-                {images_paths.map((image_path, index) => (
-                    <Image key={index} src={`/${image_path}`} alt="Car" width={MINIATURE} height={MINIATURE} className='rounded-md shadow-sm shadow-primary-blue/15' />
+            <div className='my-1 scrollable flex flex-row gap-[${MARGIN_BETWEEN_IMAGES}] w-full overflow-x-auto scroll-co'>
+                {imagesPaths.map((imagePath, index) => (
+                    <Image key={index} src={`/${imagePath}`} alt="Car" width={MINIATURE} height={MINIATURE} className={`rounded-md rounded-tr-none rounded-tl-none shadow-sm shadow-primary-blue/15 ${index === activeImgIndex ? 'border-2 border-primary-blue' : ''}`} onClick={() => handleMiniatureClick(index)}/>
                 ))}
             </div>
         </div>
