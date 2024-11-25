6b4097b02e248ec92966a8c854d7a7bf626014ec
KBRAINX <aurel.nguepssi@facsciences-uy1.cm>
2024-11-25 11:17:19 +0100

    base component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import DefaultProfile from "@/components/DefaultProfile";
+import ImageProfile from "@/components/ImageProfile";
+import Evaluation from "@/components/Evaluation";
+import Stars from "@/components/Stars";
+import Sidebar from "@/components/Sidebar";
 
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
-
+          <DefaultProfile name='John Doe'/>
+          <ImageProfile imageUrl="car.png" width={45} height={45}/>
+          <Evaluation/>
+          <Stars value={2.5} precision={0.5}/>
+          <Sidebar/>
       </main>
       <footer className="">
       </footer>
