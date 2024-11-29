171e048bdafea2e845542ca97c984b6df28404c5
Ntye Ebo'o Nina Laissa <ntye.nina@gmail.com>
2024-11-29 10:48:30 +0100

    readme update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,22 @@ import ImageProfile from "@/components/ImageProfile";
 import Evaluation from "@/components/Evaluation";
 import Stars from "@/components/Stars";
 import Sidebar from "@/components/Sidebar";
+import ThemeToggle from "@/components/base-component/Toggles";
 
 
 export default function Home() {
   return (
-    <div>
+    <>
       <main>
-          <DefaultProfile name='John Doe'/>
-          <ImageProfile imageUrl="car.png" width={45} height={45}/>
-          <Evaluation/>
-          <Stars value={2.5} precision={0.5}/>
-          <Sidebar/>
+        <ThemeToggle />
+        <DefaultProfile name='John Doe'/>
+        <ImageProfile imageUrl="car.png" width={45} height={45}/>
+        <Evaluation/>
+        <Stars value={2.5} precision={0.5}/>
+        <Sidebar/>
       </main>
       <footer>
       </footer>
-    </div>
+    </>
   );
 }
