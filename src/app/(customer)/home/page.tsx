c42232104dc9ed5324f83adfca9c7b626f20b39d
KBRAINX <aurel.nguepssi@facsciences-uy1.cm>
2024-11-29 16:41:43 +0100

    new file structure

---
+++ src/app/(customer)/home/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function Home() {
+
+    return (
+        <div>
+            <main>
+                <div>
+                    Home page
+                </div>
+            </main>
+        </div>
+    );
+}
