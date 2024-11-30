6e831256656c9d4c655c4277e79ffaa265ad95dc
cgnomc1 <cgnomc@gmail.com>
2024-11-30 10:45:12 +0100

    dernier modif avant de recuperer le reste du projet

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Image from "next/image";
   
 
@@ -39,7 +39,7 @@ const  About = () => {
                     <p className=" mb-4">
                     On her way she met a copy. The copy warned the Little Blind Text, that
                     where it came from it would have been rewritten a thousand times and
-                    everything that was left from its origin would be the word "and" and
+                    everything that was left from its origin would be the word and and
                     the Little Blind Text should turn around and return to its own, safe
                     country.
                     </p>
