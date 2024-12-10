7f55c10bfb4ccd4672a9d59607a0f06c1c36e268
Ntye Ebo'o Nina Laissa <ntye.nina@gmail.com>
2024-12-10 20:24:52 +0100

    customer help page

--- src/app/(customer)/profile/layout.tsx
+++ src/app/(customer)/profile/layout.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import ProfileSidebar from "@/components/ProfileSidebar";
 import NavBar from "@/components/organisation/NavBar";
+import Footer from "@/components/Footer";
 
 export default function Layout({children }: { children: React.ReactNode }) {
     return (
@@ -16,6 +17,8 @@ export default function Layout({children }: { children: React.ReactNode }) {
                     <div className="flex-grow overflow-y-auto bg-[F6F7F9] p-6">
                         {children}
                     </div>
+
+                    {/*<Footer/>*/}
                 </div>
             </div>
         </>
