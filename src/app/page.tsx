b65e36130fb1a418d6dfbb226f57eea850f85e4d
Ntye Ebo'o Nina Laissa <128290918+Ntye@users.noreply.github.com>
2025-01-05 21:09:01 +0100

    Update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,54 @@
-import NavOrg from "@/components/organisation/NavOrg";
-import OrgSidebar from "@/components/OrgSidebar";
-import DefaultProfile from "@/components/DefaultProfile";
-import ImageProfile from "@/components/ImageProfile";
-import Evaluation from "@/components/Evaluation";
-import Stars from "@/components/Stars";
-import Sidebar from "@/components/Sidebar";
-import ThemeToggle from "@/components/base-component/Toggles";
+
+import SportCarCard from "@/components/base-component/sportCarCard";
+import CarCarousel from "@/components/CarCarousel";
+import EarnWithUs from "@/components/earnWithUs";
+import Navbar from "@/components/organisation/NavBar";
+import Records from "@/components/Records";
+import DriverCarousel from "@/components/driverCarousel";
+import Footer from "@/components/Footer";
+import AboutUS from "@/components/about";
 
 
 export default function Home() {
   return (
     <>
-      <main>
-        <NavOrg />
-        <OrgSidebar/>
-        <ThemeToggle />
-        <DefaultProfile name='John Doe'/>
-        <ImageProfile imageUrl="car.png" width={45} height={45}/>
-        <Evaluation/>
-        <Stars value={2.5} precision={0.5}/>
-        <Sidebar/>
+      <main className="bg-var(--background)">
+        <Navbar />
+        {/* <OrgSidebar/> */}
+        {/* <ThemeToggle /> */}
+        {/* <DefaultProfile name='John Doe'/> */}
+        {/* <ImageProfile imageUrl="car.png" width={45} height={45}/> */}
+        {/* <Evaluation/> */}
+        {/* <Stars value={2.5} precision={0.5}/> */}
+        {/* <Sidebar/> */}
+        <SportCarCard/>
+        <div className="flex flex-col items-center justify-center text-xl my-12 w-full">
+            <div className="text-xl text-primary-blue">
+              What We offer
+            </div>
+            <div className="mt-4 text-secondary-text">
+              Featured Vehicles
+            </div>
+
+            <CarCarousel/>
+        </div>
+        
+        <AboutUS/>
+        <EarnWithUs/>
+        <div className="flex flex-col items-center justify-center text-xl my-12">
+            <div className="text-xl text-primary-blue">
+              What We offer
+            </div>
+            <div className="mt-4 text-secondary-text">
+              Meet ours Drivers
+            </div>
+
+            <DriverCarousel/>
+        </div>
+        <Records/>
+        
       </main>
-      <footer>
-      </footer>
+      <Footer/>
     </>
   );
 }
