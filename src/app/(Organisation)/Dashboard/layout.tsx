7631f79d67fa0de8c03bf2ba0b151da8fc2ddae0
whitecodename <ouendeufranck@gmail.com>
2025-01-08 18:47:32 +0100

    Update: Organization cars page

--- src/app/(Organisation)/Dashboard/layout.tsx
+++ src/app/(Organisation)/Dashboard/layout.tsx
@@ -1,26 +1,23 @@
 'use client';
 
 import React from "react";
-import OrgSidebar from "@/components/OrgSidebar";
-import NavOrg from "@/components/organisation/NavOrg";
+import ProfileSidebar from "@/components/ProfileSidebar";
+import NavBar from "@/components/organisation/NavBar";
 
 export default function Layout({children }: { children: React.ReactNode }) {
     return (
         <>
             <div className="flex h-screen">
-                <div>
-                    <OrgSidebar />
-                </div>
+                <ProfileSidebar/>
 
                 <div className="flex flex-col flex-grow">
-                    <NavOrg />
+                    <NavBar/>
 
                     <div className="flex-grow overflow-y-auto bg-gray-100 p-6">
                         {children}
                     </div>
                 </div>
             </div>
-
         </>
     )
 }
