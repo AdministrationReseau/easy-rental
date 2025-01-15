9ca416ea9c3fbf58c9fe3e31425306e640227412
Ntye Ebo'o Nina Laissa <ntye.nina@gmail.com>
2025-01-15 16:20:45 +0100

    responsive location page

--- src/app/(customer)/profile/layout.tsx
+++ src/app/(customer)/profile/layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({children }: { children: React.ReactNode }) {
                 <div className="flex flex-col flex-grow">
 
 
-                    <div className="flex-grow overflow-y-auto bg-gray-100 p-4 md:p-6">
+                    <div className="flex-grow overflow-y-auto bg-gray-100 p-6">
                         {children}
                     </div>
 
