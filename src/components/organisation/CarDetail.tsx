ebf3f30608f119dec44a7abd1e261879a2587815
Ntye Ebo'o Nina Laissa <ntye.nina@gmail.com>
2025-02-19 15:34:01 +0100

    updated calendars

--- src/components/organisation/CarDetail.tsx
+++ src/components/organisation/CarDetail.tsx
@@ -130,9 +130,6 @@ const VehicleInfo: React.FC<{ vehicle: CarProps }> = ({ vehicle }) => {
             ${vehicle.pricePerDay * 1.25}
           </p>
         </div>
-        <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors">
-          Book Now
-        </button>
       </div>
     </div>
   );
